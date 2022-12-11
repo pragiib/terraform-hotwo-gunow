1a5cb1baab960c470ae0112c614c7b7e2ad1dc6c
o.borzykin <borzykin3455@gmail.com>
2022-12-12 01:07:04 +0200

    Var for dir

--- user_data.sh
+++ user_data.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-sudo mkdir -p /etc/grid-router/quota/
+quota_dir=/etc/grid-router/quota
+
+sudo mkdir -p $quota_dir
 sudo htpasswd -bc /etc/grid-router/users.htpasswd company company-pass
-sudo touch /etc/grid-router/quota/test.xml
-sudo chmod 775 /etc/grid-router/quota/test.xml
-cat <<EOT >> /etc/grid-router/quota/test.xml
+sudo touch $quota_dir/test.xml
+sudo chmod 775 $quota_dir/test.xml
+
+cat <<EOT >> $quota_dir/test.xml
 <qa:browsers xmlns:qa="urn:config.company.vdr">
-<browser name="chrome" defaultVersion="88.0">
+<browser name="chrome" defaultVersion="101.0">
     <version number="101.0">
         <region name="eu-central-1">
-            <host name="selenoid.example.com" port="4444" count="1"/>
+            <host name="selenoid.example.com" port="4444" count="5"/>
         </region>
     </version>
 </browser>
 </qa:browsers>
 EOT
-sudo docker run -d --name \
-ggr -v /etc/grid-router/:/etc/grid-router:ro \
+
+sudo docker run -d --name ggr -v /etc/grid-router/:/etc/grid-router:ro \
     --net host aerokube/ggr:latest-release
